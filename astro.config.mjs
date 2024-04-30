8b1706b142894dadcd0f90a38bb7b7767856e916
Kalyan Jyoti Borah <borahkalyan@outlook.com>
2024-05-01 00:03:06 +0530

    Update Astro and its dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import markdoc from "@astrojs/markdoc";
-import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc"
+import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config'
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), markdoc(), react()],
   output: "server",
   adapter: netlify()
-});
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,10 @@ module.exports = {
       },
     },
     extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -72,9 +76,73 @@ module.exports = {
         },
       },
       animation: {
+        'tracking-in-contract-bck-bottom': 'tracking-in-contract-bck-bottom 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both',
+        'tracking-in-expand-fwd-top': 'tracking-in-expand-fwd-top 0.8s cubic-bezier(0.215, 0.61, 0.355, 1) both',
+        'kenburns-top': 'kenburns-top 5s ease-out both',
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        'scale-in-hor-center': 'scale-in-hor-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
       },
+      keyframes: {
+        'tracking-in-contract-bck-bottom': {
+          '0%': {
+            'letter-spacing': '1em',
+            '-webkit-transform': 'translateZ(400px) translateY(300px)',
+            'transform': 'translateZ(400px) translateY(300px)',
+            'opacity': '0',
+          },
+          '40%': {
+            'opacity': '0.6',
+          },
+          '100%': {
+            '-webkit-transform': 'translateZ(0) translateY(0)',
+            'transform': 'translateZ(0) translateY(0)',
+            'opacity': '1',
+          },
+        },
+        'tracking-in-expand-fwd-top': {
+          '0%': {
+            'letter-spacing': '-0.5em',
+            '-webkit-transform': 'translateZ(-700px) translateY(-500px)',
+            'transform': 'translateZ(-700px) translateY(-500px)',
+            'opacity': '0',
+          },
+          '40%': {
+            'opacity': '0.6',
+          },
+          '100%': {
+            '-webkit-transform': 'translateZ(0) translateY(0)',
+            'transform': 'translateZ(0) translateY(0)',
+            'opacity': '1',
+          },
+        },
+        'kenburns-top': {
+          '0%': {
+            '-webkit-transform': 'scale(1) translateY(0)',
+            'transform': 'scale(1) translateY(0)',
+            '-webkit-transform-origin': '50% 16%',
+            'transform-origin': '50% 16%',
+          },
+          '100%': {
+            '-webkit-transform': 'scale(1.25) translateY(-15px)',
+            'transform': 'scale(1.25) translateY(-15px)',
+            '-webkit-transform-origin': 'top',
+            'transform-origin': 'top',
+          },
+        },
+        'scale-in-hor-center': {
+          '0%': {
+            '-webkit-transform': 'scaleX(0)',
+            'transform': 'scaleX(0)',
+            'opacity': '1',
+          },
+          '100%': {
+            '-webkit-transform': 'scaleX(1)',
+            'transform': 'scaleX(1)',
+            'opacity': '1',
+          },
+        },
+      }
     },
   },
   plugins: [require("tailwindcss-animate"), require('@tailwindcss/typography')]
